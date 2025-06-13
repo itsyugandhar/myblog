954386063528a1b55a738d5a530fbbc164f6f171
itsyugandhar <yyugandhar914@gmail.com>
2025-06-13 10:46:22 +0530

    Fix NULL import issue for Linux

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
-from asyncio.windows_events import NULL
 from pickle import TRUE
 # Create your models here.
 
